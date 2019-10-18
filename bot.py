4005d12fb720e4c1c927c3603e28e0d5ad305295
Sunny Capt <sunny.capt@tuta.io>
2019-10-18 11:11:11 +0300

    switched to a new version of python-telegram-bot

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 from logging import getLogger
 
-from telegram.ext import Updater, CommandHandler
-from telegram.ext.dispatcher import run_async
+from telegram import Update
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
 from config.settings import *
 from utils import User, DB
@@ -11,19 +11,22 @@ users = User()
 db = DB()
 
 
-@run_async
-def start(bot, update):
-    logger.info(f"New request. Command start, source: {update.message.message}")
-    bot.sendMessage(
-        chat_id=update.message.chat_id,
-        text="You are gay.",
-        parse_mode="MARKDOWN"
-    )
+def start(update: Update, context):
+    logger.info(f"New request. Command start, source: {update.message.text}")
+    update.message.reply_text("You are gay.")
 
 
-@run_async
-def auth(bot, update):
+def echo(update, context):
+    update.message.reply_text(update.message.text)
+
+
+def auth(update, context):
     pass
+
+
+def error(update, context):
+    """Log Errors caused by Updates."""
+    logger.error('Update "%s" caused error "%s"', update, context.error)
 
 
 try:
@@ -34,11 +37,11 @@ except AssertionError as e:
 
 
 def run_bot():
-    global updater
     logger.info("Start polling bot")
-    updater = Updater(BOT_TOKEN, **({"request_kwargs": REQUEST_KWARGS} if REQUEST_KWARGS is not None else {}))
-    start_handler = CommandHandler("start", start)
-    updater.dispatcher.add_handler(start_handler)
+    updater = Updater(BOT_TOKEN, use_context=True, request_kwargs={'proxy_url': "socks5://127.0.0.1:9050"})  # **({"request_kwargs": REQUEST_KWARGS} if REQUEST_KWARGS is not None else {}))
+    updater.dispatcher.add_handler(CommandHandler("start", start))
+    updater.dispatcher.add_handler(MessageHandler(Filters.text, echo))
+    updater.dispatcher.add_error_handler(error)
     updater.start_polling()
     updater.idle()
 
