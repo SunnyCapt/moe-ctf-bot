9ba3c45fb26f0f1322b30cebd268756df7f0321e
SunnyCapt <42912268+SunnyCapt@users.noreply.github.com>
2019-11-12 23:39:14 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
+# in ~/path/to/moe-ctf-bot
 apt-get install git tor python3-virtualenv
-git clone https://github.com/SunnyCapt/moe-ctf-bot.git
-cd moe-ctf-bot
 virtualenv venv --no-site-packages --python=python3
 source venv/bin/activate
 pip install -r requirements.txt
